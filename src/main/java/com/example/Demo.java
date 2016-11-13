692a7cd4ec63d8bc8ee595aa3e016015453fdb78
Zongwen Li <zwli@thoughtworks.com>
2016-11-13 21:15:30 +0800

    first commit

---
+++ src/main/java/com/example/Demo.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zwli on 13/11/2016.
+ */
+@Service
+public class Demo {
+    public int Add(int a , int b)
+    {
+        return a+b;
+    }
+}
---
+++ src/test/java/com/example/DemoTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by zwli on 13/11/2016.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoTest {
+
+    @Autowired
+    private  Demo demo;
+
+    @Test
+    public void addTest() throws Exception {
+        int a = 1;
+        int b=2;
+        int expectResult = 3;
+        int actualResult = demo.Add(a,b);
+        assertThat(actualResult, is(equalTo(expectResult)));
+
+    }
+
+}
